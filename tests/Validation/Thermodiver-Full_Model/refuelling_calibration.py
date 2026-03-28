7370bb31194850980f782829d806ee68e12ae96c
Lucas Calderon <lcalderon@ethz.ch>
2026-03-28 19:52:59 +0100

    Callibrating thermodiver config sheet. Refuelling bypass definition corrected

--- tests/Validation/Thermodiver-Full_Model/refuelling_calibration.py
+++ tests/Validation/Thermodiver-Full_Model/refuelling_calibration.py
@@ -47,22 +47,24 @@ Power_TD = 336.5 # [W]
 Power_TD_isothermal = m_dot_b * R_spec * T_des * np.log(p_tank / p_orb)
 eta_refuel_isothermal = 1 / Power_TD * m_dot_b * R_spec * T_des * np.log(p_tank / p_orb)
 
-
+print(f"m_dot_b: {m_dot_b}")
 print(f"Power_TD_isothermal: {Power_TD_isothermal}")
 print(f"eta_refuel_isothermal: {eta_refuel_isothermal}")
 
 
 # Plot the value of R_spec as a function of altitude
-import matplotlib.pyplot as plt
+plot = False
+if plot:
+    import matplotlib.pyplot as plt
 
-h_orb = np.linspace(150, 500, 100)
-R_spec = np.zeros(len(h_orb))
-for i, h in enumerate(h_orb):
-    mission_data = sample_atmosphere_at_height(h)
-    R_spec[i] = mission_data.specific_gas_constant
+    h_orb = np.linspace(150, 500, 100)
+    R_spec = np.zeros(len(h_orb))
+    for i, h in enumerate(h_orb):
+        mission_data = sample_atmosphere_at_height(h)
+        R_spec[i] = mission_data.specific_gas_constant
 
-plt.plot(h_orb, R_spec)
-plt.xlabel("Altitude [km]")
-plt.ylabel("Specific Gas Constant [J/(kg*K)]")
-plt.title("Specific Gas Constant as a function of Altitude")
-plt.show()
+    plt.plot(h_orb, R_spec)
+    plt.xlabel("Altitude [km]")
+    plt.ylabel("Specific Gas Constant [J/(kg*K)]")
+    plt.title("Specific Gas Constant as a function of Altitude")
+    plt.show()
